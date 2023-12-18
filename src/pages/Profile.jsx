914605e66da38753d005e9efb6f5c019f4f1a5f4
haniekid <thuharosy2003@gmail.com>
2023-12-18 16:02:22 +0700

    :hammer: Fix profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -34,80 +34,112 @@ const Profile = () => {
           <h4 className="profile__detail-title">Thông tin cơ bản</h4>
         </div>
       </div> */}
-      <div className="profile__container_update">
-        <h2>Cập nhật thông tin cá nhân</h2>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Ảnh đại diện</Form.Label>
-            <img src={user} alt="" className="avatar-img" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>
-              Họ và tên <span>*</span>
-            </Form.Label>
-            <Form.Control type="text" placeholder="Trịnh Thu Hà" required />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>
-              Email <span>*</span>
-            </Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="thuha2003@example.com"
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Điện thoại</Form.Label>
-            <Form.Control type="phone" placeholder="0912.345.678" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Ngày sinh</Form.Label>
-            <Form.Control type="date" />
-          </Form.Group>
-          {['radio'].map((type) => (
-            <div key={`inline-${type}`} className="mb-3">
-              <Form.Check
-                inline
-                label="Nam"
-                name="group1"
-                type={type}
-                id={`inline-${type}-1`}
+      <div className="profile__main">
+        <div className="profile__container_update">
+          <h2>Thông tin cá nhân</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Ảnh đại diện</Form.Label>
+              <img src={user} alt="" className="avatar-img" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>
+                Họ và tên <span>*</span>
+              </Form.Label>
+              <Form.Control type="text" placeholder="Trịnh Thu Hà" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>
+                Email <span>*</span>
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="thuha2003@example.com"
+                required
               />
-              <Form.Check
-                inline
-                label="Nữ"
-                name="group1"
-                type={type}
-                id={`inline-${type}-2`}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Điện thoại</Form.Label>
+              <Form.Control type="phone" placeholder="0912.345.678" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Ngày sinh</Form.Label>
+              <Form.Control type="date" />
+            </Form.Group>
+            {['radio'].map((type) => (
+              <div key={`inline-${type}`} className="mb-3">
+                <Form.Check
+                  inline
+                  label="Nam"
+                  name="group1"
+                  type={type}
+                  id={`inline-${type}-1`}
+                />
+                <Form.Check
+                  inline
+                  label="Nữ"
+                  name="group1"
+                  type={type}
+                  id={`inline-${type}-2`}
+                />
+              </div>
+            ))}
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Địa chỉ</Form.Label>
+              <Form.Control type="text" placeholder="Địa chỉ" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Số CMND/CCCD</Form.Label>
+              <Form.Control type="text" placeholder="Số CMND/CCCD" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Mã thẻ BHYT</Form.Label>
+              <Form.Control type="text" placeholder="Mã thẻ BHYT" />
+            </Form.Group>
+            <Button
+              as="input"
+              type="submit"
+              value="Cập nhật"
+              className="profile-btn"
+            />{' '}
+            <Button
+              as="input"
+              type="button"
+              value="Hủy bỏ"
+              className="profile-btn cancel"
+            />{' '}
+          </Form>
+        </div>
+
+        <div className="profile__password">
+          <h2>Thay đổi mật khẩu</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>
+                Mật khẩu hiện tại <span>*</span>
+              </Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Mật khẩu hiện tại của bạn"
+                required
               />
-            </div>
-          ))}
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Địa chỉ</Form.Label>
-            <Form.Control type="text" placeholder="Địa chỉ" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Số CMND/CCCD</Form.Label>
-            <Form.Control type="text" placeholder="Số CMND/CCCD" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Mã thẻ BHYT</Form.Label>
-            <Form.Control type="text" placeholder="Mã thẻ BHYT" />
-          </Form.Group>
-          <Button
-            as="input"
-            type="submit"
-            value="Cập nhật"
-            className="profile-btn"
-          />{' '}
-          <Button
-            as="input"
-            type="button"
-            value="Hủy bỏ"
-            className="profile-btn cancel"
-          />{' '}
-        </Form>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>
+                Mật khẩu mới <span>*</span>
+              </Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Nhập mật khẩu mới"
+                required
+              />
+            </Form.Group>
+            <a href="#">Quên mật khẩu</a>
+            <Button variant="primary" type="submit" className="btn-change">
+              Thay đổi
+            </Button>
+          </Form>
+        </div>
       </div>
       <FooterClient />
     </div>
